980c46e980bad1b6865dfdd6c8f25d10f44e0ad1
Ye Yang <1126439586@qq.com>
2020-08-14 14:13:20 +0800

    修改（优化）二分查找法

--- src/算法/09.二分查找法.js
+++ src/算法/09.二分查找法.js
@@ -4,25 +4,27 @@
 
 */
 
-function findindex(arr, target){
-  const len = arr.length
-  let l = 0, r = len
-  let midIndex = Math.floor(len/2)
-  while(l <= r){
-    let middleValue = arr[midIndex]
-    if(middleValue === target) return midIndex
-    if(middleValue > target){
-      r = midIndex - 1
-      midIndex = Math.floor((r - l)/2 + l)
-    }else {
-      l = midIndex + 1
-      midIndex = Math.floor((r - l)/2 + l)
+Array.prototype.myFindIndex = function find(target){
+    let len = this.length
+    let midIndex = Math.floor(len/2)
+    let l = 0, r = len
+    while(l <= r){
+        let midIndex = Math.floor((l+r)/2)
+        let midValue = this[midIndex]
+        if(midValue === target) return midIndex
+        if(midValue > target){
+            r = midIndex
+        }else{
+            l = midIndex + 1
+        }
     }
-  }
   return -1
 }
 
 // 测试用例
-let arr = Array.from({length: 1000}, (item, index) => index+1)
-let target = 999
-console.log(findindex(arr, target))
+let arr = Array.from({length: 100}, (item, index) => index+1)
+let target = 99
+console.time('myFindIndex')
+let res = arr.myFindIndex(target)
+console.timeEnd('myFindIndex')
+console.log(res) // 98
