630a3508037515f526450812cde51be9da9fa719
Ye Yang <1126439586@qq.com>
2020-09-28 08:44:17 +0800

    😊新增常用布局方案；新增一些js基本算法

--- src/算法/20.[#14]寻找最长公共前缀.js
+++ src/算法/20.[#14]寻找最长公共前缀.js
@@ -25,7 +25,7 @@ const longestCommonPreFix1 = (strs) => {
   return longestFix
 }
 
-// 纵向扫描法
+// 纵向扫描法1
 const longestCommonPreFix2 = (strs) => {
   const len = strs.length
   if (len === 0) {
@@ -58,5 +58,43 @@ const longestCommonPreFix2 = (strs) => {
   return preFix
 }
 
-let strList = ['flower', 'fly', 'f']
-console.log(longestCommonPreFix2(strList))
+// 纵向扫描法2
+const longestCommonPreFix3 = (strs) => {
+  const len = strs.length
+  if(len === 0 ) return ""
+  if(len === 1) return strs[0]
+  let preFix = ""
+  const current = strs[0]
+  const currentLen = current.length
+  let isEqual = true
+  for(let i = 0; i < currentLen; i++){ // 第i个字符
+    for(let j = 1; j < len; j++){ // 第j个单词a
+      if(strs[j][i] !== current[i]){
+        isEqual = false
+        break
+      }
+    }
+    if(isEqual) preFix += current[i]
+  }
+  return preFix
+}
+
+// 纵向扫描法3
+const longestCommonPreFix4 = (strs) => {
+  let re = ""
+  const len = strs.length
+  if(len === 0) return re
+  if(len === 1) return strs[0]
+  for(let i = 0; i < strs[0].length; i++){
+    for(let j = 1; j < len; j++){
+      if(strs[j][i] !== strs[0][i]){
+        return re
+      }
+    }
+    re += strs[0][i]
+  }
+  return re
+}
+
+let strList = ['flower', 'fly', 'flo']
+console.log(longestCommonPreFix4(strList))
