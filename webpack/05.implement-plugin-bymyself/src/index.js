8c371daed059afc0937c44d13fd9a2f771fd5f8f
YeYang <1126439586@qq.com>
2021-03-05 21:22:44 +0800

    完成自定义plugin案例

--- webpack/05.implement-plugin-bymyself/src/index.js
+++ webpack/05.implement-plugin-bymyself/src/index.js
@@ -1,1 +1,3 @@
+// 这是一条注释
+
 console.log("let's write a webpack plugin yy")
--- webpack/05.implement-plugin-bymyself/webpack.config.js
+++ webpack/05.implement-plugin-bymyself/webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path')
+const WebpackPlugin1 = require('./plugins/basic-plugin')
+const CleanWebpackPlugin = require('./plugins/clean-webpack-plugin')
 const CopyrightWebpackPlugin = require('./plugins/copyright-webpack-plugin')
+const FileListPlugin = require('./plugins/file-list-plugin')
+const LifeCirclePlugin = require('./plugins/life-circle-plugin')
+const RemoveCommentPlugin = require('./plugins/remove-comment-plugin')
 
 module.exports = {
   mode: 'development',
@@ -24,6 +29,11 @@ module.exports = {
     path: path.resolve(__dirname, './dist')
   },
   plugins: [
-    new CopyrightWebpackPlugin()
+    // new CopyrightWebpackPlugin(), // ! 生成新的文件的plugin，compilation.assets['copyright.txt'] = xxxxxxx
+    // new WebpackPlugin1({msg: 'Hello World!'})
+    // new LifeCirclePlugin(), // ! 查看钩子声明周期
+    // new FileListPlugin(),
+    // new CleanWebpackPlugin() // ! 测试失败
+    new RemoveCommentPlugin()
   ]
 }
