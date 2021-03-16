c1cd87755935b2d6558fddd5cd012293838da2f4
YeYang <1126439586@qq.com>
2021-03-16 23:50:03 +0800

    🧐webpack的splitChunks测试

--- React/03.react-router实现原理/webpack.config.js
+++ React/03.react-router实现原理/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { TerserPlugin } = require('html-minifier-terser')
 
 module.exports = {
   entry: './src/index.tsx',
@@ -28,6 +29,16 @@ module.exports = {
         }
       }
     ]
+  },
+  optimization: {
+    minimizer: [
+      // new TerserPlugin()
+    ],
+    chunkIds: 'deterministic', // natural自然数；named以文件名+路径组织见名知意；deterministic生产环境推荐，同文件不变
+    splitChunks: {
+      chunks: 'initial', // ! import 动态导入的模块都会默认做代码分离
+      maxSize: 300000
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
