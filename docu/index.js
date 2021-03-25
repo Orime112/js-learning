a5881939c4d04ad06f42261d8652912fe4cb9a6d
Orime小猪 <1126439586@qq.com>
2021-03-25 20:20:49 +0800

    🍧完善合并有序数组

--- docu/index.js
+++ docu/index.js
@@ -6,16 +6,20 @@ function mergeArr(arr1, arr2){ return [...arr1, ...arr2].sort((a, b) => a -b) }
 function mergeArr1(arr1, arr2){
   let a = [...arr1]
   let b = [...arr2]
-  const first = a[0] > b[0] ? b.shift() : a.shift()
-  const res = [first]
-  while(a[0] < b[0]){
+  let res = []
+  while(a.length){
+    if(!b.length) {
+      res.push(...[...a, ...b])
+      return res
+    }
     // a 4 b 2 res 1
     if(a[0] > b[0]){
       res.push(b.shift())
     } else {
-      res.push(a.shift)
+      res.push(a.shift())
     }
   }
+  res.push(...[...a, ...b])
   return res
 }
 
